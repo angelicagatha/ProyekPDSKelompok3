867cfb5b66700a4c628209f6e4a91b267881b129
Angelica Agatha <125953295+angelicagatha@users.noreply.github.com>
2023-06-02 15:01:20 +0700

    login OK

--- user/loginproses.php
+++ user/loginproses.php
@@ -11,6 +11,7 @@
     $result = $conn->query($query);
 
     if($result->num_rows>0){
+        $_SESSION['email'] = $email;
         header("location: homeuser.php");
     } else {
         echo "Username atau password salah";
