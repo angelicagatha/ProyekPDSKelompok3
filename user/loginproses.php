f1ceef35250644e98bc444d0779edbd9a1f95c56
Angelica Agatha <125953295+angelicagatha@users.noreply.github.com>
2023-06-02 14:43:58 +0700

    login signup logout

---
+++ user/loginproses.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'koneksi.php';
+
+    session_start();
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $hashed_password = hash("md5", $password);
+
+    $query = "SELECT * FROM user WHERE email = '$email' AND password = '$hashed_password'";
+    $result = $conn->query($query);
+
+    if($result->num_rows>0){
+        header("location: homeuser.php");
+    } else {
+        echo "Username atau password salah";
+    }
+
+    $conn->close();
+?>
---
+++ user/signupproses.php
@@ -0,0 +1,46 @@
+<?php
+  require_once 'koneksi.php';
+
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $email = $_POST['email'];
+            $nama_user = $_POST['nama_user'];
+            $password = $_POST['password'];
+        ?>
+            <?php
+                if ($email == '' || $nama_user == '' || $password == ''){
+                    echo '<i class="fas fa-times-circle fa-7x" style="color: red"></i>';
+                    echo '<h3 style="margin-top: 1%;">Post gagal direquest!</h3>';
+
+                    if ($email == '') {
+                        echo 'email invalid <br>';
+                    }
+
+                    if ($nama_user == '') {
+                        echo 'nama_user invalid <br>';
+                    }
+                    
+                    if ($password == '') {
+                        echo 'password invalid <br>';
+                    }
+                } else {
+                    $query = "INSERT INTO user (email, nama_user, password)
+                                VALUES ('". $email."', '". $nama_user."', '". $password."')";
+                    $conn->query($query);
+                }
+            ?>
+
+        <script>
+            alert('Selamat, Data berhasil diupload!');
+            <?php
+                // header('Location: loginuser.php');
+                // exit;
+            ?>
+        </script>
+    </body>
+</html>
