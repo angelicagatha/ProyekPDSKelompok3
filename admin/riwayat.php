1ccf31250727784d2ea4cfd0e13e74e56624eb40
Angelica Agatha <125953295+angelicagatha@users.noreply.github.com>
2023-06-16 16:35:03 +0700

    Update riwayat.php

---
+++ admin/riwayat.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Riwayat</title>
+        <style>
+        .book-card {
+          width: 300px;
+          border: 1px solid #ccc;
+          border-radius: 5px;
+          padding: 20px;
+          margin: 10px;
+          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+          height: auto;
+        }
+
+        .book-title {
+            font-size: 18px;
+            font-weight: bold;
+            margin-bottom: 10px;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+
+        .book-author {
+            font-size: 14px;
+            color: #555;
+            margin-bottom: 10px;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+
+        .book-description {
+            font-size: 14px;
+            margin-bottom: 10px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 3;
+            -webkit-box-orient: vertical;
+            height: 60px; /* Atur tinggi sesuai kebutuhan */
+        }
+
+        .book-cover {
+            width: 100%;
+            height: auto;
+            margin-bottom: 10px;
+        }
+
+        .book-list {
+          display: grid;
+          grid-template-columns: repeat(4, 1fr);
+          grid-gap: 20px;
+        }
+
+        ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+          background-color: #333;
+        }
+
+        li {
+          float: left;
+        }
+
+        li a {
+          display: block;
+          color: white;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+        }
+
+        li a:hover:not(.active) {
+          background-color: #111;
+        }
+
+        .active {
+          background-color: #04AA6D;
+        }
+      </style>
+    </head>
+    <body>
+        <ul>
+            <li><a class="active" href="#">Home</a></li>
+            <li><a href="cart.php?idUser=<?php echo $id_user ?>">Cart</a></li>
+            <li><a href="logoutuser.php">Logout</a></li>
+            <li class="active" style="float:right"><a href="#">Welcome, <?php echo $username?>!</a></li>
+        </ul>
+    </body>
+</html>
